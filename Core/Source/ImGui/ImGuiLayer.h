72ea2d9ee31961e6cc675c3d34957cd350598c74
Alex <lepsalex@gmail.com>
2024-02-17 20:42:43 -0500

    reformat ImGui

--- Core/Source/ImGui/ImGuiLayer.h
+++ Core/Source/ImGui/ImGuiLayer.h
@@ -3,25 +3,25 @@
 #include "Core/Layer.h"
 
 namespace Sophon {
-class ImGuiLayer : public Layer {
-public:
-    ImGuiLayer();
-    ~ImGuiLayer() = default;
+    class ImGuiLayer : public Layer {
+    public:
+        ImGuiLayer();
+        ~ImGuiLayer() = default;
 
-    virtual void OnAttach() override;
-    virtual void OnDetach() override;
-    virtual void OnEvent(Event& e) override;
+        virtual void OnAttach() override;
+        virtual void OnDetach() override;
+        virtual void OnEvent(Event& e) override;
 
-    void Begin();
-    void End();
+        void Begin();
+        void End();
 
-    void BlockEvents(bool block) { m_BlockEvents = block; }
+        void BlockEvents(bool block) { m_BlockEvents = block; }
 
-    void SetDarkThemeColors();
+        void SetDarkThemeColors();
 
-    uint32_t GetActiveWidgetID() const;
+        uint32_t GetActiveWidgetID() const;
 
-private:
-    bool m_BlockEvents = true;
-};
+    private:
+        bool m_BlockEvents = true;
+    };
 }
