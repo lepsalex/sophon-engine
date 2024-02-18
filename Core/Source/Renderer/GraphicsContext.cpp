0e7dc6dbdd69b496b149e40f36535694d7907eba
Alex <lepsalex@gmail.com>
2024-02-17 20:39:02 -0500

    create context based om RendererAPI

--- Core/Source/Renderer/GraphicsContext.cpp
+++ Core/Source/Renderer/GraphicsContext.cpp
@@ -1,12 +1,22 @@
 #include "sfnpch.h"
 
+#include "Renderer/Renderer.h"
 #include "Renderer/GraphicsContext.h"
+
 #include "Platform/OpenGL/OpenGLContext.h"
 
 namespace Sophon {
     Scope<GraphicsContext> GraphicsContext::Create(void* window)
     {
-        // TODO: This method could return other types of contexts (Vulkan, DirectX, etc)
-        return CreateScope<OpenGLContext>(static_cast<GLFWwindow*>(window));
+        switch (Renderer::GetAPI()) {
+        case RendererAPI::API::None:
+            SFN_CORE_ASSERT(false, "RendererAPI::None is not currently supported!");
+            return nullptr;
+        case RendererAPI::API::OpenGL:
+            return CreateScope<OpenGLContext>(static_cast<GLFWwindow*>(window));
+        }
+
+        SFN_CORE_ASSERT(false, "Unknown RendererAPI!");
+        return nullptr;
     }
 }
