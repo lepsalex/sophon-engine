a575483c9e7f8227ab0ac4da84c9973b8b436129
Alex <lepsalex@gmail.com>
2024-02-24 11:23:42 -0500

    update shader.h to include a lib and a new create from file method

--- Core/Source/Renderer/Shader.h
+++ Core/Source/Renderer/Shader.h
@@ -18,6 +18,22 @@ namespace Sophon {
 
         virtual const std::string& GetName() const = 0;
 
+        static Ref<Shader> Create(const std::string& filepath);
         static Ref<Shader> Create(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc);
+    };
+
+    class ShaderLibrary {
+    public:
+        void Add(const std::string& name, const Ref<Shader>& shader);
+        void Add(const Ref<Shader>& shader);
+        Ref<Shader> Load(const std::string& filepath);
+        Ref<Shader> Load(const std::string& name, const std::string& filepath);
+
+        Ref<Shader> Get(const std::string& name);
+
+        bool Exists(const std::string& name) const;
+
+    private:
+        std::unordered_map<std::string, Ref<Shader>> m_Shaders;
     };
 }
