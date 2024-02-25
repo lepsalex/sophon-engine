ffb82b37af80118febd89b4b4cc3cb43f395b714
Alex <lepsalex@gmail.com>
2024-02-25 16:50:53 -0500

    Renderer2D Minor Improvements (#6)
    * Renderer2D small refactor
    
    * testing out the API changes

--- App/Source/Layers/Sandbox2D.cpp
+++ App/Source/Layers/Sandbox2D.cpp
@@ -11,8 +11,8 @@ void Sandbox2D::OnAttach()
     SFN_PROFILE_FUNCTION();
 
     auto textureOptions = Sophon::TextureOptions {
-        Sophon::MinMagFilter::NEAREST,
-        Sophon::MinMagFilter::NEAREST,
+        Sophon::MinMagFilter::LINEAR,
+        Sophon::MinMagFilter::LINEAR,
         Sophon::WrapOption::REPEAT,
         Sophon::WrapOption::REPEAT,
     };
@@ -43,8 +43,8 @@ void Sandbox2D::OnUpdate(Sophon::Timestep ts)
     SFN_PROFILE_SCOPE("Render Draw");
     Sophon::Renderer2D::BeginScene(m_CameraController.GetCamera());
     Sophon::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-    Sophon::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-    Sophon::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, m_CheckerboardTexture);
+    Sophon::Renderer2D::DrawRotatedQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, 45.0f, { 0.2f, 0.3f, 0.8f, 1.0f });
+    Sophon::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, m_CheckerboardTexture, 5.0f, { 0.9f, 0.8f, 0.7f, 1.0f });
     Sophon::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.1f }, { 1.0f, 1.0f }, m_LogoTexture);
     Sophon::Renderer2D::EndScene();
 }
--- Core/Source/Renderer/Renderer2D.h
+++ Core/Source/Renderer/Renderer2D.h
@@ -13,26 +13,14 @@ namespace Sophon {
         static void BeginScene(const OrthographicCamera& camera);
         static void EndScene();
 
-        static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
-        {
-            DrawQuad(position, size, 0.0f, color);
-        }
-        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
-        {
-            DrawQuad(position, size, 0.0f, color);
-        }
-        static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture)
-        {
-            DrawQuad(position, size, 0.0f, texture);
-        }
-        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture)
-        {
-            DrawQuad(position, size, 0.0f, texture);
-        }
+        static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color);
+        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color);
+        static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
 
-        static void DrawQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color);
-        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color);
-        static void DrawQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture);
-        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture);
+        static void DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color);
+        static void DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color);
+        static void DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+        static void DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
     };
 }
